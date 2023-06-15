3477ffe25b6e19a8a4fb70aca151ca249fa69650
kato <130636687+kato110op@users.noreply.github.com>
2023-06-15 14:01:41 +0900

    復讐問題

--- fukuv0615/Form1.cs
+++ fukuv0615/Form1.cs
@@ -2,9 +2,39 @@ namespace fukuv0615
 {
     public partial class Form1 : Form
     {
+        int ans = rand.Next(101);
+        int count = 0;
+        int a = 0;
+        static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int guess;
+            guess= $"{TextBox}";
+            Text = $"{count}";
+            count = (count) + 1;
+            MessageBox.Show("{count}âΩâÒñ⁄ÇÃíßêÌ");
+            if (ans == guess)
+            {
+                MessageBox.Show("Bingo");
+            }
+            else if (ans < guess)
+            {
+                MessageBox.Show("Less");
+            }
+            if (ans > guess)
+            {
+                MessageBox.Show("Greater");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
